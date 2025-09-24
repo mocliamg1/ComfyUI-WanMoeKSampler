f82259518598b1f9de7edbd51153e9d6b23ce401
mocliamg1 <121634570+mocliamg1@users.noreply.github.com>
2025-09-24 18:38:34 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
-from .nodes import WanMoeKSampler,WanMoeKSamplerAdvanced,SplitSigmasAtT
+from .nodes import WanMoeKSampler,WanMoeKSamplerAdvanced
 
 NODE_CLASS_MAPPINGS = {
     "WanMoeKSampler3pass":WanMoeKSampler,
-    "WanMoeKSamplerAdvanced3pass":WanMoeKSamplerAdvanced,
-    "SplitSigmasAtT":SplitSigmasAtT
+    "WanMoeKSamplerAdvanced3pass":WanMoeKSamplerAdvanced
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "WanMoeKSampler3pass": "Wan 3pass MoE KSampler",
-    "WanMoeKSamplerAdvanced3pass": "Wan 3pass MoE KSampler (Advanced)",
-    "SplitSigmasAtT": "Split sigmas at timestep"
+    "WanMoeKSamplerAdvanced3pass": "Wan 3pass MoE KSampler (Advanced)"
 }
